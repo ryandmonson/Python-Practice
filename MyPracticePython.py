6ee0882c0101544418c4011aa8d0fe576209dcf5
Ryan Monson <ryandmonson@gmail.com>
2021-09-21 12:12:28 -0400

    Update Python practice

--- MyPracticePython.py
+++ MyPracticePython.py
@@ -31,12 +31,18 @@ def multiples (multiple, limit):
     print(myList)
 multiples (2,6)
 
+#Complete the function that takes two integers(a, b, where a < b) and return
+#an array of all integers between the input parameters, including them.
+
+
 
 # Convert and return USD to Yuan
 
 def usdcny(usd):
     yuan = 6.75 * usd
-    return '{:0.2f} Chinese Yuan'.format(round(yuan,2))
+    print ('{:0.2f} Chinese Yuan'.format(round(yuan,2)))
+    return yuan
+usdcny(1)
 
 
 #Timmy & Sarah think they are in love, but around where they live, 
@@ -53,4 +59,26 @@ def lovefunc(flower1, flower2):
     else:
         return False
 
-print (lovefunc(1,2))
+print((lovefunc(1,2)))
+
+# Create a function that accepts 2 string arguments and returns 
+# an integer of the count of occurrences the 2nd argument is found in the first one.
+
+def string_char (string,char):
+    count = 0
+    for i in string:
+        if i == char:
+            count += 1
+    return count
+print(string_char(" ", "l"))
+
+
+#Given 2 strings, a and b, return a string of the form short+long+short, 
+#with the shorter string on the outside and the longer string on the inside. 
+#The strings will not be the same length, but they may be empty ( zero length ).
+
+def shortlong (a,b):
+    if (len(a) > len(b)):
+        return (b + a + b)
+    return (a + b + a)
+print(shortlong("1", "22"))
